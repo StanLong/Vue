ac62be659f2a49565829f2596d2533fa79836792
StanLong <youlinbhz@sina.cn>
2019-12-28 20:54:05 +0800

    webpack配置

--- Vue/webpack-study/src/main.js
+++ Vue/webpack-study/src/main.js
@@ -1,11 +1,25 @@
 // 项目js的入口文件
-
-// 1、导入jquery
+// 安装 jquery 模块 npm i jquery 
 // import *** from *** 是 es6 中导入模块的方式
 import $ from 'jquery'
 
+// 使用import 语法导入css样式表
+// 处理非 js 文件，需要手动安装第三方合适的 loader 加载器
+// 1、npm i style-loader css-loader -D
+// 2、再webpack.config.js这个配置文件里面新增一个配置节点，叫做module，它是一个对象
+// 再这个 module 对象上有一个rules属性的数组，存放了第三方的文件和匹配的处理规则
+import './css/index.css'
+
+// npm i less -D
+// npm i less-loader -D
+import './css/index.less'
+
+// npm i node-sass -D
+// npm i sass-loader -D
+import './css/index.scss'
+
 $(function(){
-    $('li:odd').css('backgroundColor', 'lightblue')
+    $('li:odd').css('backgroundColor', 'green')
     $('li:even').css('backgroundColor', function(){
         return '#' + 'D97634'
     })
---
+++ Vue/webpack-study/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+// 启用热更新
+const webpack = require('webpack') // wbpack4启用热更新的不需要配置
+// 导入在内存中生成html页面的插件, 只有是插件都一定要梵高 plugins 节点中去
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+
+// 通过node中的模块操作，向外暴露了一个配置对象
+module.exports={
+    // 在配置文件中需要手动指定webpack打包的入口和出口
+    entry: path.join(__dirname, './src/main.js'), // 指定webpack打包的文件
+    output:{
+        path: path.join(__dirname, './dist'), // 输出的文件目录
+        filename:'bundle.js' // 指定输出的文件名称
+    },
+    devServer:{ // 配置 dev-server 命令参数的第二种方式
+        // --open --port 3000 --contentBase src --hot
+        open: true, // 自动打开浏览器
+        port:3000, // 设置启动时候的运行端口
+        contentBase:'src', // 指定托管的根目录
+        hot:true // 启用热更新
+    },
+    plugins:[ // 配置插件的节点 // wbpack4启用热更新的不需要配置
+        new webpack.HotModuleReplacementPlugin(), // new 一个热更新的模块对象
+        new htmlWebpackPlugin({ // 创建一个在内存中生成html的插件
+            template:path.join(__dirname, './src/index.html'), // 指定在内存中生成的模板页面
+            filename:'index.html' // 指定生成的页面名称
+        })
+    ],
+    module:{ // 用于配置所有的第三方模块加载器
+        rules:[ // 第三方模块的匹配规则
+            {test: /\.css$/, use:['style-loader', 'css-loader']}, // 配置处理 .css 文件的第三方loader,从右往左调用
+            {test: /\.less$/, use:['style-loader', 'css-loader', 'less-loader']}, // 配置处理 .less 文件的第三方loader,从右往左调用
+            {test: /\.scss$/, use:['style-loader', 'css-loader', 'sass-loader']} // 配置处理 .scss 文件的第三方loader,从右往左调用
+
+        ]
+
+    }
+}
